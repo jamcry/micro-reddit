b67330d9b960bdf86403ae9079460c1c495d238f
jamcry <jamcry@hotmail.com>
2019-05-02 15:48:25 +0300

    Add format validation for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	validates :name, presence: true, length: { minimum: 3, maximum: 10}
-	validates :email, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
 	has_many :posts
 	has_many :comments
