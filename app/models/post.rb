981a5eaf3e530f43d96d5387dfe3d89691ba4282
jamcry <jamcry@hotmail.com>
2019-05-02 15:29:41 +0300

    Create Post model and add associations

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 5, maximum: 20}
+	validates :body, 	presence: true, length: { maximum: 240 }
+
+	belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 	validates :name, presence: true, length: { minimum: 3, maximum: 10}
 	validates :email, presence: true
+
+	has_many :posts
 end
